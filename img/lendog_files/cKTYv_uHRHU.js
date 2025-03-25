53735a147089bee183486ccf456a48d7c4d8e9a9
jranks123 <jonnyrankin@googlemail.com>
2025-03-25 16:15:18 +0000

    earlybitd

---
+++ img/lendog_files/cKTYv_uHRHU.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("LsMessageClickFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("1743853");b=d("FalcoLoggerInternal").create("ls_message_click",a);e=b;g["default"]=e}),98);
